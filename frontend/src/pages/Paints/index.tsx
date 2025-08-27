ecd294dc8b0731b407a39b3730717b15cb632e3d
kiansajjadi <kian.s.sajjadi@gmail.com>
2025-08-28 01:35:19 +1000

    Add crunkalunka gredient don't go asking about the secret ingredient

--- frontend/src/pages/Paints/index.tsx
+++ frontend/src/pages/Paints/index.tsx
@@ -9,13 +9,19 @@ type Paint = {
   sku?: string;
 };
 
+type Column = keyof Paint
+
 // Placeholder - will get this from db at some point
 const paints: Paint[] = [
   {colour_name:"hexed lichen", manufacturer: "Vallejo", vessel: "dropper", range: "game colour"},
   {colour_name:"black", manufacturer: "Vallejo", vessel: "dropper", range: "model colour"},
   {colour_name:"hexed lichen", manufacturer: "Vallejo", vessel: "dropper", range: "game colour"},
+  {colour_name:"hexed lichen", range: "game colour", manufacturer: "Vallejo", vessel: "dropper"},
   {colour_name:"hexed lichen", manufacturer: "Vallejo", vessel: "dropper", range: "game colour"},
-  {colour_name:"hexed lichen", manufacturer: "Vallejo", vessel: "dropper", range: "game colour"},
+]
+
+const columns: Column[] = [
+  "colour_name", "manufacturer", "vessel", "range", "sku", "opacity"
 ]
 
 export default function Paints() {
@@ -23,19 +29,18 @@ export default function Paints() {
     <Table.Root size="3">
       <Table.Header>
         <Table.Row>
-          <Table.ColumnHeaderCell><Text>Colour Name</Text></Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell><Text>Manufacturer</Text></Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell><Text>Vessel Type</Text></Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell><Text>Range</Text></Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell><Text>Opacity</Text></Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell><Text></Text>SKU</Table.ColumnHeaderCell>
-        </Table.Row>
+        {columns.map((column) => (
+            <Table.ColumnHeaderCell>
+              <Text>{column}</Text>
+            </Table.ColumnHeaderCell>
+        ))}
+      </Table.Row>
       </Table.Header>
       <Table.Body>
         {paints.map((paint) => (
           <Table.Row>
-            {Object.entries(paint).map(([key, value]) => (
-              <Table.Cell key={key}>{value}</Table.Cell>
+            {columns.map((column) => (
+              <Table.Cell key={column}>{paint[column]}</Table.Cell>
             ))}
           </Table.Row>
         ))}
